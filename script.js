59cebc230f3d9aab22b691114ff0afa8fed9a699
Mahadi176 <mahadi210176@gmail.com>
2026-02-23 03:07:12 +0600

    new updates

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let thrivingList = []
 let strugglingList = []
+let currentStatus =''
 
 let total = document.getElementById('total')
 let thrivingCount = document.getElementById('thrivingCount')
@@ -19,6 +20,8 @@ function calculateCount(){
 calculateCount()
 
 function toggleStyle(id){
+    
+
     if(id == 'thriving-filter-btn'){
         allCardSection.classList.add('hidden')
         filterSection.classList.remove('hidden')
@@ -61,7 +64,40 @@ mainContainer.addEventListener('click',function(event){
         thrivingList.push(cardInfo)
 
     }
+    calculateCount()
+
     renderThriving()
+    }
+    else if(event.target.classList.contains('struggling-btn')){
+        const parenNode = event.target.parentNode.parentNode
+
+    const plantName = parenNode.querySelector('.plantName').innerText
+    const latinName = parenNode.querySelector('.latinName').innerText
+    const  light    = parenNode.querySelector('.light').innerText
+    const  water    = parenNode.querySelector('.water').innerText
+    const  status   = parenNode.querySelector('.status').innerText
+    const  notes    = parenNode.querySelector('.notes').innerText
+
+    parenNode.querySelector('.status').innerText = 'Struggle'
+
+        const cardInfo ={
+         plantName,
+         latinName,
+         light,
+         water,
+         status:'Struggle',
+         notes
+        }
+    const plantExist = strugglingList.find(item => item.plantName == cardInfo.plantName)
+
+    if(!plantExist){
+
+        strugglingList.push(cardInfo)
+
+    }
+    calculateCount()
+
+    renderStruggling()
     }
     
 })
@@ -84,6 +120,46 @@ function renderThriving(){
        </div>
        <div class=" mb-4">
            <p class="status w-40 border  rounded-lg px-2">${thrive.status}</p>
+           <p class="notes mt-2">New Leaf unfurling by the east window</p>
+       </div>
+       <div>
+           <button class="thrivingBtn p-2 rounded-md bg-green-200 text-green-800">
+                                    Thriving
+           </button>
+           <button class="strugglingBtn p-2 rounded-md bg-red-200 text-red-800">
+                                    Struggling
+           </button>
+        </div>
+        </div>
+        <!-- part-2  -->
+        <div>
+        <button class="btnDelete p-2 rounded-md text-red-800 border border-red-800">
+                                      Delete
+        </button>
+       </div>
+            `
+            filterSection.appendChild(div)
+        }  
+
+}
+function renderStruggling(){
+        filterSection.innerHTML = ''
+        for(let struggle of strugglingList){
+            let div = document.createElement('div')
+            div.className = "card flex justify-between border border-black p-4 rounded-md my-5"
+            div.innerHTML = `      
+        <!-- part-1  -->
+        <div>
+        <div>
+           <p class="plantName text-xl font-bold ">${struggle.plantName}</p>
+           <p class="latinName text-gray-600">Latin Name</p>
+       </div>
+       <div class="flex gap-4 my-4">
+           <p class="light bg-slate-200 rounded-lg px-2">Bright Indicate</p>
+           <p class="water bg-slate-200 rounded-lg px-2">Weekly</p>
+       </div>
+       <div class=" mb-4">
+           <p class="status w-40 border  rounded-lg px-2">${struggle.status}</p>
            <p class="notes mt-2">New Leaf unfurling by the east window</p>
        </div>
        <div>
